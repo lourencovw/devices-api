723ee1be19e3b1ddecb67c1431dda8373c668fa5
Lourenço Viguini Werneck <lourencovw@gmail.com>
2025-11-17 17:34:02 -0300

    feat: implement initial device model, controller, factory, and migration

---
+++ database/factories/device_factory.ts
@@ -0,0 +1,8 @@
+import factory from '@adonisjs/lucid/factories'
+import Device from '#models/device'
+
+export const DeviceFactory = factory
+  .define(Device, async ({ faker }) => {
+    return {}
+  })
+  .build()
