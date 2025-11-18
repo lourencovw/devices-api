0008a2a53ef707721667ec5e8ca2988f84442e5c
Lourenco <lourencovw@gmail.com>
2025-11-17 23:11:01 -0300

    feat: enhance routing to include Swagger documentation for devices

--- start/routes.ts
+++ start/routes.ts
@@ -7,8 +7,21 @@
 |
 */
 
-import DevicesController from '#controllers/devices_controller'
+const DevicesController = () => import('#controllers/devices_controller')
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from "adonis-autoswagger";
+import swagger from "#config/swagger";
 
-router.resource('devices', DevicesController)
+router.resource('devices', DevicesController).apiOnly()
 
+// returns swagger in YAML
+router.get("/swagger", async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger);
+});
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get("/docs", async () => {
+  return AutoSwagger.default.ui("/swagger", swagger);
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead. If you want, you can pass proxy url as second argument here.
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+});
